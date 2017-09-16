5e93e7315afa18c17e529b9a619546d71a274714
Benedict Etzel <benedict@hearthsim.net>
2017-09-16 22:27:09 +0200

    Add generic HOC factory

---
+++ src/utils/hocs.tsx
@@ -0,0 +1,29 @@
+import * as React from "react";
+import { capitalize } from "./strings";
+import { ValidationMap } from "prop-types";
+
+/**
+ * Use this method to create a simple HOC to directly extract some props from the context.
+ */
+export const makeHOC = <TInjectProps extends {}>(
+	contextTypes: ValidationMap<any>,
+) => <TExternalProps extends {}>(
+	WrappedComponent: React.ComponentClass<TExternalProps & TInjectProps>,
+) =>
+	class GenericHOC extends React.Component<TExternalProps, {}> {
+		static displayName = `${Object.keys(contextTypes)
+			.map(capitalize)
+			.join("")}HOC`;
+		static contextTypes = contextTypes;
+
+		render() {
+			const contextVars: { [k: string]: string } = {};
+			for (const contextKey in contextTypes) {
+				contextVars[contextKey] = this.context[contextKey];
+			}
+			Object.keys(contextTypes).map(k => this.context[k]);
+			const { children, ...props } = this.props as any;
+			const newProps = Object.assign({}, contextVars, props);
+			return React.createElement(WrappedComponent, newProps, children);
+		}
+	};
---
+++ src/utils/strings.ts
@@ -0,0 +1,5 @@
+/**
+ * Capitalizes the first letter of the given string.
+ */
+export const capitalize = (string: string) =>
+	string.charAt(0).toUpperCase() + string.slice(1);
