95dfd3cd8f1d9d18fcd4523e32a9316cbd1b3231
Benedict Etzel <benedict@hearthsim.net>
2017-09-28 19:52:54 +0200

    Add hero power placeholder

--- src/viewer/placeholders/index.ts
+++ src/viewer/placeholders/index.ts
@@ -2,6 +2,7 @@ import MinionPlaceholder from "./minion.png";
 import SpellPlaceholder from "./spell.png";
 import WeaponPlaceholder from "./weapon.png";
 import HeroPlaceholder from "./hero.png";
+import HeroPowerPlaceholder from "./hero_power.png";
 
 const getPlaceholder = (type: string): string => {
 	switch (type) {
@@ -11,6 +12,8 @@ const getPlaceholder = (type: string): string => {
 			return WeaponPlaceholder;
 		case "HERO":
 			return HeroPlaceholder;
+		case "HERO_POWER":
+			return HeroPowerPlaceholder;
 		default:
 		case "MINION":
 			return MinionPlaceholder;
